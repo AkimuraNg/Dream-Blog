0394629832f748e4ebb0d80f39b9351c0a9a39a6
HNDuy Nguyen <duynguyen11062001@gmail.com>
2022-12-10 13:30:24 +0700

    Install json-server, edit home page css

--- dream/src/components/Home.js
+++ dream/src/components/Home.js
@@ -20,20 +20,23 @@ const Home = () => {
             <br /> Then this website is for you! </h4>
           <p>Introducing 'Dream blog'. A webpage where you can share you dream with others without having to revealing yourself.
             Here, on this website, everyone is anonymous and the only thing that is shown is their dream stories.
-            You have a good dream last night? Share it on here!
-            You have a scary nightmare? Share it on here as well! We accept all kind of dreams and nightmares that you experienced.
+            <br/>You have a good dream last night? Share it on here!
+            <br/>You have a scary nightmare? Share it on here as well! 
+            <br/>We accept all kind of dreams and nightmares that you experienced.
           </p>
+          <p>Multiverse theory</p>
+          
         </section>
 
         {/* Features view */}
-        <section style={{ paddingTop: 120 }} id="scrollspyFeats" className="featuresSection" data-bs-spy="scroll" data-bs-target="#navbarContent" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabindex="-1">
+        <section style={{ paddingTop: 120 }} id="scrollspyFeats" className="featuresSection" data-bs-spy="scroll" data-bs-target="#navbarContent" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true">
           <h1 style={{ textAlign: 'right', paddingRight: 40 }}>Website Features</h1>
-          <h4 style={{ textAlign: 'right', paddingRight: 40 }}>Here are some features that we have at the moment</h4>
+          <h4 style={{ textAlign: 'right', paddingRight: 40 }}>Here are some features that the website currently supports</h4>
           {/* Card views */}
           <div className="featItems" style={{ padding: 80 }}>
             {feats.map(post => {
               return (
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card" style={{ width: "18rem" }}> 
                   <img src={post.image} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title" style={{ textAlign: 'center', paddingBottom: 20 }}>{post.title}</h5>
