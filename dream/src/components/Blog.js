be86e23c92f8f45ffa366332d680405b61daf5ec
HNDuy Nguyen <duynguyen11062001@gmail.com>
2023-01-25 16:09:27 +0200

    Implemeting dark/light

--- dream/src/components/Blog.js
+++ dream/src/components/Blog.js
@@ -5,32 +5,62 @@ const Blog = () => {
     return (
         <div className="container-fluid">
             <main className="wrapper">
-               <section style={{paddingTop: 80, textAlign: 'center'}}>
+                <div className="Blog" style={{ display: 'flex', flexDirection: 'row' }}>
+                    <section className="blogHead">
+                        <div className="row">
+                            <h1>Blog Page</h1>
+                            <p>Here you can view other stories and publish your own.</p>
+                        </div>
+                    </section>
+                    <section className="blogItems">
+                        <div className="card">
+                            <div className="card-body" style={{ textAlign: 'center' }}>
+                                <h4 className="card-title"> Some title</h4>
+                                <p className="card-text">Some paragraph</p>
+                            </div>
+                            <button className="btn" style={{ width: '40%', alignSelf: 'center' }}>Read More</button>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-body" style={{ textAlign: 'center' }}>
+                                <h4 className="card-title"> Some title</h4>
+                                <p className="card-text">Some paragraph</p>
+                            </div>
+                            <button className="btn" style={{ width: '40%', alignSelf: 'center' }}>Read More</button>
+                        </div>
+
+                    </section>
+                </div>
+                {/* <section style={{paddingTop: 80, paddingLeft: 100, textAlign: 'center'}}>
                     <h1>Blog Page</h1>
                     <p>Here you can view other stories and publish your own.</p>
                </section>
-               <section style={{display: 'flex', paddingTop: 20}}>
+               <section className="blogItems" style={{paddingLeft: 100}}>
+                    <div className="card" style={{width: '70%'}}>
+                        <div className="card-body">
+                            <h4 className="card-title"> Some title</h4>
+                            <p className="card-text">Some paragraph</p>
+                        </div>
+                        <button className="btn-primary"> Read More</button>
+                    </div>
                     <div className="card" style={{width: '18rem'}}>
+                        
+                    </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        
+                    </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        
+                    </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        
+                    </div>
+                    <div className="card" style={{width: '18rem'}}>
+                        
+                    </div>
+               </section> */}
 
-                    </div>
-                    <div className="card" style={{width: '18rem'}}>
-                        
-                    </div>
-                    <div className="card" style={{width: '18rem'}}>
-                        
-                    </div>
-                    <div className="card" style={{width: '18rem'}}>
-                        
-                    </div>
-                    <div className="card" style={{width: '18rem'}}>
-                        
-                    </div>
-                    <div className="card" style={{width: '18rem'}}>
-                        
-                    </div>
-               </section>
-
-               {/* <button style={{borderRadius: "70%"}}>
+                {/* <button style={{borderRadius: "70%"}}>
                     <img src={plus} alt="plus"/>
                </button> */}
             </main>
