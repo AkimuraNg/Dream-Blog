2f0ee1976a06d8231f58ec136493caa4ca818034
HNDuy Nguyen <duynguyen11062001@gmail.com>
2023-02-18 15:54:53 +0200

    modify blog page

---
+++ dream/src/components/AddNew.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddNew = () => {
+  return (
+    <div className="container-fluid">
+        <main className="wrapper">
+            <h1 style={{paddingTop: 120, textAlign: 'center'}}>Create a new story</h1>
+        </main>
+    </div>
+  )
+}
+
+export default AddNew
